22bf177da327d3aa31ad146000a2022ba6df121e
Ashley Park <apark2@brynmawr.edu>
2021-12-02 12:38:52 -0500

    some of format

--- format.c
+++ format.c
@@ -1,1 +1,78 @@
+#include "format.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
 
+#define MEGABYTE 1024 * 1024
+
+int main(int argc, char** argv)
+{
+    int num_megabytes = 1;
+
+    if (argc == 1)//too few arguments
+    {
+        printf("too few arguments\n");
+        exit(EXIT_FAILURE);
+    }else if (strcmp(argv[1], "DISK") != 0)//file name must be disk
+    {
+        printf("file name must be disk\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (argc == 3)//1: file command, 2: disk name, 3: flag, 3: size of file in MB
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    if (argc > 3)
+    {
+        if (strcmp(argv[2], "-s") == 0)
+        {
+            num_megabytes = atoi(argv[3]);
+        }else
+        {
+            printf("%s is not an accepted flag\n", argv[2]);
+        }
+        
+    }
+
+    void* disk = malloc(MEGABYTE*num_megabytes+1);
+
+    /*super->size;
+    super->inode_offset;//inode region
+    super->data_offset;//data region
+    super->swap_offset;//end of file
+    super->free_inode;//link to first inode
+    super->free_block;//link to head of free block list*/
+
+    void* ptr = disk;
+    ptr += 512;
+    superblock* super = (superblock*)ptr;
+    super->size = 512;
+    super->inode_offset = 1;
+
+
+
+    FILE* fp = fopen("DISK", "w");
+    int check = ftruncate(fileno(fp), MEGABYTE*num_megabytes);
+    
+    
+    superblock* block = (superblock*)(disk + 512);
+
+    void* for_inode = disk;
+    for_inode += 1024 + 1 * 512;//bootblock + superblock + blocksize * inode_offset
+
+    inode* node = (inode*)for_inode;
+    
+
+    printf("%d\n", block->size);
+    
+
+    if (fp != NULL && check == 0)
+    {
+        printf("SUCCESS\n");
+    }
+}
--- format.h
+++ format.h
@@ -1,1 +1,33 @@
 
+#define N_DBLOCKS 10
+#define N_IBLOCKS 4
+
+typedef struct superblock 
+{
+    int size;
+    int super_user;
+    int regular_user;
+    int inode_offset;
+    int data_offset;
+    int swap_offset;
+    int free_inode;
+    int free_block;
+}superblock;
+
+typedef struct inode {
+    char* filename[28];
+    int next_inode;
+    int protect;
+    int nlink;
+    int size;
+    int uid;
+    int gid;
+    int ctime;
+    int mtime;
+    int atime;
+    int dblocks[N_DBLOCKS];
+    int iblocks[N_IBLOCKS];
+    int i2block;
+    int i3block;
+}inode;
+
