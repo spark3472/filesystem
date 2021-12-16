ce5067a223e9a01d32d72e8136c1ffab0dd55fbb
maya-j <mayaruthbar@gmail.com>
2021-12-16 16:02:31 +0000

    formatting changes

--- vfs.h
+++ vfs.h
@@ -21,10 +21,6 @@
 
 enum {OREAD, ORDWR, OWRITE, OCREAT, OAPPEND, ORDAD};
 
-//f_open returns file handle
-//f_opendir returns directory handle
-
-
 //stat struct taken from man fstat()
 struct stat_t {
     int inode;
@@ -33,57 +29,21 @@ struct stat_t {
     int gid;
     int size;
     int blocksize;
-    int permissions;
+    int permissions;    //chmod permissions
     int mtime;
 };
-
-/*struct stat {
-    //dev_t     st_dev;     /* ID of device containing file */
-    //ino_t     st_ino;     /* inode number */
-    //mode_t    st_mode;    /* protection */
-    //nlink_t   st_nlink;   /* number of hard links */
-    //uid_t     st_uid;     /* user ID of owner */
-    //gid_t     st_gid;     /* group ID of owner */
-    //dev_t     st_rdev;    /* device ID (if special file) */
-    //off_t     st_size;    /* total size, in bytes */
-    //blksize_t st_blksize; /* blocksize for file system I/O */
-    //blkcnt_t  st_blocks;  /* number of 512B blocks allocated */
-    //time_t    st_atime;   /* time of last access */
-    //time_t    st_mtime;   /* time of last modification */
-    //time_t    st_ctime;   /* time of last status change */
-//};
 
 typedef struct vnode {
     int disk;
     char name[255];
     struct vnode* child;
     struct vnode* next;
-    int permissions;
-    int chmod;
+    int permissions;    //holds read/write permissions
+    int chmod;          //holds chmod permissions
     int type;
     //fs_driver_t* driver;  
     int inode;			/* hard coding for unix */
 } vnode_t;
-
-typedef struct fs_driver{
-    int				    (*f_open)(char *path, char* filename, int flag);
-    size_t 				(*f_read)(void *ptr, size_t size, int num, int fd);
-    size_t 				(*f_write)(vnode_t *vn, void *data, size_t size, int num, int fd);
-    int 				(*f_close)(int fd);
-    int 				(*f_seek)(vnode_t *vn, int offset, int whence, int fd);
-    int 				(*f_rewind)(vnode_t *vn, int fd);
-    int					(*f_stat)(vnode_t *vn, struct stat_t *buf, int fd);
-    int					(*f_remove)(vnode_t *vn, const char *filename);
-    int				    (*f_opendir)(vnode_t *vn, const char *filename);
-    struct _DirEntry*	(*f_readdir)(vnode_t *vn, int* directory);
-    int					(*f_closedir)(vnode_t *vn, int* directory);
-    int					(*f_mkdir)(vnode_t *vn, const char *filename, int mode);
-    int					(*f_rmdir)(vnode_t *vn, const char *filename);
-    int					(*f_mount)(char* src, char* target);
-    int					(*f_umount)(vnode_t *vn, const char *dir, int flags);
-}fs_driver_t;
-
-
 
 // to track opened files
 typedef struct fileEntry{
@@ -99,15 +59,6 @@ typedef struct dirent{
     int where;
     
 }dirent;
-
-/*struct dirent {
-        //ino_t          d_ino;       /* Inode number */
-        //off_t          d_off;       /* Not an offset; see below */
-        //unsigned short d_reclen;    /* Length of this record */
-        //unsigned char  d_type;      /* Type of file; not supported
-                                        //by all filesystem types */
-        //char           d_name[256]; /* Null-terminated filename */
-//};
 
 dirent *dirTable[MAX_DT_SIZE];
 
@@ -152,4 +103,36 @@ int change_chmod(char* path, int chmod);
 
 
 
+/*typedef struct fs_driver{
+    int				    (*f_open)(char *path, char* filename, int flag);
+    size_t 				(*f_read)(void *ptr, size_t size, int num, int fd);
+    size_t 				(*f_write)(vnode_t *vn, void *data, size_t size, int num, int fd);
+    int 				(*f_close)(int fd);
+    int 				(*f_seek)(vnode_t *vn, int offset, int whence, int fd);
+    int 				(*f_rewind)(vnode_t *vn, int fd);
+    int					(*f_stat)(vnode_t *vn, struct stat_t *buf, int fd);
+    int					(*f_remove)(vnode_t *vn, const char *filename);
+    int				    (*f_opendir)(vnode_t *vn, const char *filename);
+    struct _DirEntry*	(*f_readdir)(vnode_t *vn, int* directory);
+    int					(*f_closedir)(vnode_t *vn, int* directory);
+    int					(*f_mkdir)(vnode_t *vn, const char *filename, int mode);
+    int					(*f_rmdir)(vnode_t *vn, const char *filename);
+    int					(*f_mount)(char* src, char* target);
+    int					(*f_umount)(vnode_t *vn, const char *dir, int flags);
+}fs_driver_t;*/
 
+/*struct stat {
+    //dev_t     st_dev;     /* ID of device containing file */
+    //ino_t     st_ino;     /* inode number */
+    //mode_t    st_mode;    /* protection */
+    //nlink_t   st_nlink;   /* number of hard links */
+    //uid_t     st_uid;     /* user ID of owner */
+    //gid_t     st_gid;     /* group ID of owner */
+    //dev_t     st_rdev;    /* device ID (if special file) */
+    //off_t     st_size;    /* total size, in bytes */
+    //blksize_t st_blksize; /* blocksize for file system I/O */
+    //blkcnt_t  st_blocks;  /* number of 512B blocks allocated */
+    //time_t    st_atime;   /* time of last access */
+    //time_t    st_mtime;   /* time of last modification */
+    //time_t    st_ctime;   /* time of last status change */
+//};
